383bac8aab23a8f90a52c0d21c61f223efa4b543
bshapir <bshapir@siue.edu>
2017-02-16 19:13:11 -0600

    Add files via upload

---
+++ Project1_CS312.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <iomanip>
+
+using namespace std;
+
+
+
+int main()
+{
+	ifstream inputFile;
+	int sizeRow;
+	int sizeCol;
+	inputFile.open("sample.txt");
+	inputFile >> sizeRow;
+	inputFile >> sizeCol;
+	inputFile.get();
+
+	char *matrix = new char[sizeRow*sizeCol];
+	for (int row = 0; row < sizeRow; row++)
+	{
+		for (int col = 0; col < sizeCol; col++)
+		{
+			inputFile >> *(matrix + sizeCol * row + col);
+		}
+	}
+
+	for (int row = 0; row < sizeRow; row++) 
+	{
+		for (int col = 0; col < sizeCol; col++)
+		{
+			cout << *(matrix + sizeCol * row + col) << " ";
+		}
+		cout << endl;
+	}
+
+	system("pause");
+
+	delete[]matrix;
+
+		return 0;
+
+		
+}
